c68bd3e1f7bd2ed668abd8ba826b1e0a61a29eae
Matteo <matteo.violani@studio.unibo.it>
2024-03-20 09:57:05 +0100

    feat: implemented task part 1 lists

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -3,6 +3,8 @@ package u03
 import u02.AnonymousFunctions.l
 import u03.Optionals.Optional
 
+import scala.annotation.tailrec
+
 object Sequences: // Essentially, generic linkedlists
   
   enum Sequence[E]:
@@ -15,24 +17,39 @@ object Sequences: // Essentially, generic linkedlists
       case Cons(h, t) => h + sum(t)
       case _          => 0
 
-    def map[A, B](l: Sequence[A])(mapper: A => B): Sequence[B] = l match
-      case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
-      case Nil()      => Nil()
+    def map[A, B](l: Sequence[A])(mapper: A => B): Sequence[B] = flatMap(l)(v => Cons(mapper(v), Nil()))
 
     def filter[A](l1: Sequence[A])(pred: A => Boolean): Sequence[A] = l1 match
-      case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
-      case Cons(_, t)            => filter(t)(pred)
-      case Nil()                 => Nil()
+        case Cons(h, t) if pred(h) => flatMap(Cons(h, filter(t)(pred)))(v => Cons(v, Nil()))
+        case Cons(_, t)            => filter(t)(pred)
+        case Nil()                 => Nil()
 
     // Lab 03
-    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = ???
+    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = (first, second) match
+        case (Cons(fh, ft), Cons(sh, st)) => Cons((fh, sh), zip(ft, st))
+        case (Nil(), _) | (_, Nil()) | (Nil(), Nil()) => Nil()
 
-    def take[A](l: Sequence[A])(n: Int): Sequence[A] = ???
+    def take[A](l: Sequence[A])(n: Int): Sequence[A] = l match
+      case Cons(h, t) if n > 0 => Cons(h, take(t)(n-1))
+      case _ => Nil()
     
-    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = ???
-    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = ???
+    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = (l1, l2) match
+      case (Nil(), _) => l2
+      case (Cons(h, Nil()), _) => Cons(h, l2)
+      case (Cons(h, l1b), l2) => Cons(h, concat(l1b, l2))
 
-    def min(l: Sequence[Int]): Optional[Int] = ???
+    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = l match
+      case Cons(h, t) => concat(mapper(h), flatMap(t)(mapper))
+      case _ => Nil()
+
+    def min(l: Sequence[Int]): Optional[Int] =
+      @tailrec
+      def minRec(l: Sequence[Int], m: Optional[Int]): Optional[Int] = l match
+        case Cons(h, t) if h <= Optional.orElse(m, h) => minRec(t, Optional.Just(h))
+        case Cons(_, t) => minRec(t, m)
+        case Nil() => m
+      minRec(l, Optional.Empty())
+
     
 @main def trySequences =
   import Sequences.* 
